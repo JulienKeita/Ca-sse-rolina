94816dfecce45f16bdb59919cff687c00ad35c41
Kikucheeseful <gregorybizet@gmail.com>
2016-03-19 16:49:18 +0100

    save before major change try

--- app/models/invites.js
+++ app/models/invites.js
@@ -6,47 +6,53 @@ var invitesSchema = new mongoose.Schema({
     description: String,
     preferences: String,
     allergies: String,
-    commentaires: String
+    commentaires: String,
+    recette: {
+        nom: String,
+        ingredients: String,
+        preparation: String,
+        avis: String
+    }
 });
 
 var Invites = {
-    
+
     model: mongoose.model('Invites', invitesSchema),
-    
-    create: function(req, res) {
-		Invites.model.create({
-			description: req.body.description,
+
+    create: function (req, res) {
+        Invites.model.create({
+            description: req.body.description,
             preferences: req.body.preferences,
             allergies: req.body.allergies,
             commentaires: req.body.commentaires
-            
-		}, function(){
-			res.sendStatus(200);
-		})
-	},
 
-	findAll: function(req, res) {
-		Invites.model.find(function (err, data) {
-			res.send(data);
-		});
-	},
+        }, function () {
+            res.sendStatus(200);
+        })
+    },
 
-	update: function(req, res){
-		Invites.model.findByIdAndUpdate(req.params.id, {
-			description: req.body.description,
+    findAll: function (req, res) {
+        Invites.model.find(function (err, data) {
+            res.send(data);
+        });
+    },
+
+    update: function (req, res) {
+        Invites.model.findByIdAndUpdate(req.params.id, {
+            description: req.body.description,
             preferences: req.body.preferences,
             allergies: req.body.allergies,
             commentaires: req.body.commentaires
-		}, function(){
-			res.sendStatus(200);
-		})
-	},
+        }, function () {
+            res.sendStatus(200);
+        })
+    },
 
-	delete: function(req, res){
-		Invites.model.findByIdAndRemove(req.params.id, function(){
-			res.sendStatus(200);
-		})
-	} 
+    delete: function (req, res) {
+        Invites.model.findByIdAndRemove(req.params.id, function () {
+            res.sendStatus(200);
+        })
+    }
 }
 
 module.exports = Invites;
